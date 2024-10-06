9db38f3b84bd45a4b782e2b750dc3bcf2cd0542d
niko <r.niko.stahl@gmail.com>
2024-10-06 17:53:09 -0400

    Implemented basic building blocks

--- draw.js
+++ draw.js
@@ -2,15 +2,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the canvas element
     const imageCanvas = document.getElementById('image_canvas');
+    const kaleidoscopeCanvas = document.getElementById('kaleidoscope_canvas');
+    const tempCanvas = document.getElementById('temp_canvas');
+
     // source image
     let img = new Image();
+    let tileImageData;
+    const tileWidth = 200;
 
     // Check if the browser supports canvas
     if (imageCanvas.getContext) {
-        loadImage(document.getElementById('image_canvas'));
+        setupKaleidoscope(kaleidoscopeCanvas);
+        loadImage(imageCanvas);
         
     } else {
         console.log('Canvas is not supported in this browser.');
+    }
+
+    function setupKaleidoscope(canvas) {
+        console.log('Setup kaleidoscope canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = "rgb(200 0 0)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
     function loadImage(canvas) {
@@ -27,6 +40,26 @@ document.addEventListener('DOMContentLoaded', function() {
         img.src = "trees.jpeg";
     }
 
+    function drawKaleidoscope(canvas) {
+        console.log('Drawing kaleidoscope');
+        const ctx = canvas.getContext('2d');
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const centerX = Math.floor(canvas.width / 2);
+        const centerY = Math.floor(canvas.height / 2);
+
+        // Create a temporary canvas to hold the tile image
+        const tempCtx = tempCanvas.getContext('2d');
+        tempCtx.putImageData(tileImageData, 0, 0);
+
+        ctx.save();
+        ctx.translate(centerX, centerY);
+        ctx.drawImage(tempCanvas, 0, 0);
+        ctx.scale(1, -1);
+        ctx.drawImage(tempCanvas, 0, 0);
+        ctx.restore();
+    }   
+
     function drawClipping(canvas, x, y) {
         console.log('Drawing clipping');
 
@@ -40,12 +73,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Create a clipping path
         ctx.beginPath();
-        ctx.arc(x, y, 60, 0, 2 * Math.PI);
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + tileWidth, y);
+        ctx.arc(x, y, tileWidth, 0, Math.PI / 8);
+        ctx.closePath();
         ctx.clip();
-
-
         // Draw the image
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+
+        // Get the clipped image data
+        tileImageData = ctx.getImageData(x, y, tileWidth, tileWidth);
 
         // Restore the canvas state
         ctx.restore();
@@ -57,6 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
             drawClipping(canvas, x, y);
+            drawKaleidoscope(kaleidoscopeCanvas);
         });
     }
 
