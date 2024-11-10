ae43f04313ded1025095a6f8b538ee9e490921b0
niko <r.niko.stahl@gmail.com>
2024-11-09 19:50:52 -0500

    Add pond

--- draw.js
+++ draw.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let touchEndY = 0;
     let mouseStartY = 0;
     let mouseEndY = 0;
-    const imageUrls = ["trees.jpeg", "leafs.jpg", "ny-bay.jpg", 
+    const imageUrls = ["pond.jpg", "trees.jpeg", "leafs.jpg", "ny-bay.jpg", 
                         "cold-spring.jpg", "hudson.jpg", "waterfall.png"]; // Add all your image URLs here
     let currentImageIndex = 0;
 
